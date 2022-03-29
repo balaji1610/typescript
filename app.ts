bc0147bc035cc328b62e517bc525f70c19754f3b
balaji1610 <balajiece1610>
2022-03-29 23:10:06 +0530

    Git Pull

--- app.ts
+++ app.ts
@@ -38,209 +38,182 @@ var Name2v = "Hello " + personData.Name + "is " + personData.Age + " old ";
 
 console.log(Name2v);
 
-
-var myCountry : {
-
-  Name:string;
-  Captial:string;
-  Pm:string;
-}
-
+var myCountry: {
+  Name: string;
+  Captial: string;
+  Pm: string;
+};
 
 myCountry = {
-  Name:"India",
-  Captial:"Delhi",
-  Pm:"Modi"
+  Name: "India",
+  Captial: "Delhi",
+  Pm: "Modi",
+};
 
-  
-}
-
-
-console.log(myCountry.Captial+" is a Captial Of "+myCountry.Name+" Our Prime Minister Name "+myCountry.Pm);
+console.log(
+  myCountry.Captial +
+    " is a Captial Of " +
+    myCountry.Name +
+    " Our Prime Minister Name " +
+    myCountry.Pm
+);
 
 /* ----------------------   object END        ------------------- */
 
-
 /* --------------  String Tools START  ---------------- */
 
+var nameData1: string = "Hello Balaji";
 
-var nameData1:string = "Hello Balaji";
+var nameData2: string = " Angular  Developer";
 
-var nameData2 :string = " Angular  Developer"
+console.log(nameData1.charAt(1)); // charAt()
 
-
-console.log(nameData1.charAt(1));     // charAt()
-
-console.log (nameData1.indexOf("B"));  // indexOf()
-
+console.log(nameData1.indexOf("B")); // indexOf()
 
 console.log(nameData1.concat(nameData2)); // concat()
 
-console.log (nameData1.concat(" Node js Developer "," & ",nameData2 ));
+console.log(nameData1.concat(" Node js Developer ", " & ", nameData2));
 
+console.log(nameData1.replace("Balaji", "Alex")); // replace()
 
-console.log (nameData1.replace("Balaji","Alex"));  // replace()
+console.log(nameData1.replace("Balaji", nameData2));
 
-console.log (nameData1.replace("Balaji",nameData2));
+var fruits: string = "Apple,Banana,Orange,Graphs";
 
-var fruits:string = "Apple,Banana,Orange,Graphs";   
+console.log(fruits.split(",")); // split()
 
-console.log (fruits.split(","));                 // split()
+console.log(fruits.split(",", 2));
 
-console.log (fruits.split(",",2))
+console.log(nameData1.toLowerCase()); //toLowerCase()
 
-console.log (nameData1.toLowerCase())        //toLowerCase()
-
-console.log (nameData1.toUpperCase())           // toUpperCase()
+console.log(nameData1.toUpperCase()); // toUpperCase()
 /* --------------  String Tools END  ---------------- */
-
 
 /* ---------------- Array START  ----------------- */
 
+let seriesName2: string[]; // Square Bracket Array
+seriesName2 = ["Rio", "Denver", "Professor"];
 
+console.log("Square Bracket Array " + seriesName2[0]);
 
-let seriesName2:string[]                       // Square Bracket Array 
-seriesName2= ["Rio","Denver","Professor"];
+let seriesName3: Array<string>; // Generic Type Array
+seriesName3 = ["Professor", "Rio", "Denver"];
 
-console.log ("Square Bracket Array "+seriesName2[0])
+console.log("Generic Type Array " + seriesName3[1]);
 
+let seriesName: Array<string>;
+seriesName = ["Rio", "Denver", "Professor"];
 
-let seriesName3:Array<string>               // Generic Type Array 
-seriesName3 = ["Professor","Rio","Denver"];
-
-console.log ("Generic Type Array "+seriesName3[1])
-
-let seriesName:Array<string>
-seriesName= ["Rio","Denver","Professor"];
-
-console.log ("Good Morning "+seriesName[2]);
+console.log("Good Morning " + seriesName[2]);
 
 // Access Array Loop
 
-let friends:string[] = ["Alex","Balaji","Chitra","Durai"];
+let friends: string[] = ["Alex", "Balaji", "Chitra", "Durai"];
 
-for (var list in friends){
-
-  console.log ("Hello "+friends[list]+" Access Array Loop")
+for (var list in friends) {
+  console.log("Hello " + friends[list] + " Access Array Loop");
 }
 
-for (var i=0 ; i<friends.length ; i++){
-
-  console.log ("Hi "+friends[i]);
+for (var i = 0; i < friends.length; i++) {
+  console.log("Hi " + friends[i]);
 }
 
+let friends2: string[] = ["Rio", "Raqul", "Denver", "Narobi"];
+let friends3: string[] = ["Rio", "Raqul", "Denver", "Narobi"];
+let friends4: string[] = ["Rio", "Raqul", "Denver", "Narobi"];
+let friends5: string[] = ["Rio", "Raqul", "Denver", "Narobi"];
+let friends6: string[] = ["Rio", "Raqul", "Denver", "Narobi"];
 
-let friends2:string[] = ["Rio","Raqul","Denver","Narobi"]
-let friends3:string[] = ["Rio","Raqul","Denver","Narobi"]
-let friends4:string[] = ["Rio","Raqul","Denver","Narobi"]
-let friends5:string[] = ["Rio","Raqul","Denver","Narobi"]
-let friends6:string[] = ["Rio","Raqul","Denver","Narobi"]
-
-friends2.push("Last Element Add");  // push() 
+friends2.push("Last Element Add"); // push()
 
 console.log(friends2);
 
 friends3.unshift("Frist Element Add"); // unshift ()
 
-console.log (friends3);
+console.log(friends3);
 
-friends4.pop();   // pop()
+friends4.pop(); // pop()
 
-console.log(friends4)
+console.log(friends4);
 
-friends5.shift();  // shift()
+friends5.shift(); // shift()
 
 console.log(friends5);
 
-friends6 = friends6.concat(["Professor","Berlin"]);
+friends6 = friends6.concat(["Professor", "Berlin"]);
 
-console.log(friends6)
+console.log(friends6);
 
+var text: string = "Hello Balaji What Are hell Doing ?";
 
-var  text:string  = "Hello Balaji What Are hell Doing ?";
-
-
-console.log(text.indexOf('Balaji'));   // indexOf()
-
-
+console.log(text.indexOf("Balaji")); // indexOf()
 
 /* ---------------- Array  END  ----------------- */
 
 /* ---------------- Tuple  START  ----------------- */
 
-var employee:[number,string] = [1,"Alex"];
+var employee: [number, string] = [1, "Alex"];
 
 console.log(employee);
 
 console.log(employee[0]);
 
-//------ Tuple Array 
+//------ Tuple Array
 
-var employee2:[number,string,number] []
-employee2 = [ [1,"Balaji",201],[2,"Alex",202]]
+var employee2: [number, string, number][];
+employee2 = [
+  [1, "Balaji", 201],
+  [2, "Alex", 202],
+];
 
-console.log(employee2 ) 
+console.log(employee2);
 
-console.log(employee2[0])
-console.log(employee2[0][1])
+console.log(employee2[0]);
+console.log(employee2[0][1]);
 
 /* ---------------- Tuple  END  ----------------- */
-
 
 /* ---------------  Enum START -------------- */
 
 enum days {
-
   mon = 1,
   tues,
   wed,
   thus,
   fri,
   sat = "Happy Saturday",
-  sun ="Happy Sunday"
-
+  sun = "Happy Sunday",
 }
 
-function listDays(parameter:any):days{
-
-if(parameter == days.mon)
-{
-return days.sun
-}else{
-  return days.sat
+function listDays(parameter: any): days {
+  if (parameter == days.mon) {
+    return days.sun;
+  } else {
+    return days.sat;
+  }
 }
- }
 
-console.log(listDays(2))
+console.log(listDays(2));
 
-console.log(days)
+console.log(days);
 /* ---------------  Enum End -------------- */
-
 
 /* ----------  Union START ----------- */
 
+let dataDetails: string | number;
 
-let dataDetails:(string | number)
+console.log((dataDetails = 10));
 
-console.log(dataDetails = 10)
+console.log((dataDetails = "Hello Union"));
 
-console.log(dataDetails ="Hello Union")
-
-
-function displayData (parameter:(string | number)){
-
-
-  if(typeof(parameter)==="string"){
-
+function displayData(parameter: string | number) {
+  if (typeof parameter === "string") {
     console.log("Generated String");
-  }else if(typeof(parameter)==="number"){
-
-    console.log("Generated Number")
-  }else {
-
-console.log("Nothing Generated ")
+  } else if (typeof parameter === "number") {
+    console.log("Generated Number");
+  } else {
+    console.log("Nothing Generated ");
   }
-
 }
 displayData("hello");
 displayData(12);
@@ -249,38 +222,31 @@ displayData(12);
 
 /* ----------  Void START ----------- */
 
-function sayHi():void{
-console.log("Hello Void")
-
+function sayHi(): void {
+  console.log("Hello Void");
 }
-console.log(sayHi())
+console.log(sayHi());
 
-function sayHi2():void{
- return
-  
-  }
-   console.log(sayHi2())
+function sayHi2(): void {
+  return;
+}
+console.log(sayHi2());
 
 /* ----------  Void END ----------- */
 
-
 /* ----- Never START --------- */
 
-const generateError = (message:string,code:number):never=>{
+const generateError = (message: string, code: number): never => {
+  throw {
+    messageCode: message,
+    errorCode: code,
+  };
+};
 
-throw {
+generateError(" An Error Occured !", 201);
 
-  messageCode:message,
-  errorCode:code
-}
-}
-
-generateError(" An Error Occured !",201);
-
-
-const result = generateError(" An Error Occured !",201);
-console.log(result)                                         //return any value
-
+const result = generateError(" An Error Occured !", 201);
+console.log(result); //return any value
 
 /*
 const getError = (message:string):never =>{
@@ -305,7 +271,8 @@ console.log("Am a Infiniteloop")
  }
  }
  infiniteLoop();
-*/
 
+ 
+*/
 
 /* ------------ Never End ------ */
