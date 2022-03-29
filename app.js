2ad3ea4caf4be800686075fb8b8bca6c4f4ed1e3
balaji1610 <balajiece1610@gmail.com>
2022-03-29 13:31:17 +0530

    Array Tools

--- app.js
+++ app.js
@@ -42,4 +42,37 @@ console.log(fruits.split(",")); // split()
 console.log(fruits.split(",", 2));
 console.log(nameData1.toLowerCase()); //toLowerCase()
 console.log(nameData1.toUpperCase()); // toUpperCase()
-/* --------------  String Tools END  ---------------- */ 
+/* --------------  String Tools END  ---------------- */
+/* ---------------- Array START  ----------------- */
+var seriesName2; // Square Bracket Array 
+seriesName2 = ["Rio", "Denver", "Professor"];
+console.log("Square Bracket Array " + seriesName2[0]);
+var seriesName3; // Generic Type Array 
+seriesName3 = ["Professor", "Rio", "Denver"];
+console.log("Generic Type Array " + seriesName3[1]);
+var seriesName;
+seriesName = ["Rio", "Denver", "Professor"];
+console.log("Good Morning " + seriesName[2]);
+// Access Array Loop
+var friends = ["Alex", "Balaji", "Chitra", "Durai"];
+for (var list in friends) {
+    console.log("Hello " + friends[list] + " Access Array Loop");
+}
+for (var i = 0; i < friends.length; i++) {
+    console.log("Hi " + friends[i]);
+}
+var friends2 = ["Rio", "Raqul", "Denver", "Narobi"];
+var friends3 = ["Rio", "Raqul", "Denver", "Narobi"];
+var friends4 = ["Rio", "Raqul", "Denver", "Narobi"];
+var friends5 = ["Rio", "Raqul", "Denver", "Narobi"];
+friends2.push("Last Element Add"); // push() 
+console.log(friends2);
+friends3.unshift("Frist Element Add"); // unshift ()
+console.log(friends3);
+friends4.pop(); // pop()
+console.log(friends4);
+friends5.shift(); // shift()
+console.log(friends5);
+var text = "Hello Balaji What Are hell Doing ?";
+console.log(text.indexOf('Balaji')); // indexOf()
+/* ---------------- Array  END  ----------------- */ 
--- app.ts
+++ app.ts
@@ -93,3 +93,74 @@ console.log (nameData1.toLowerCase())        //toLowerCase()
 
 console.log (nameData1.toUpperCase())           // toUpperCase()
 /* --------------  String Tools END  ---------------- */
+
+
+/* ---------------- Array START  ----------------- */
+
+
+
+let seriesName2:string[]                       // Square Bracket Array 
+seriesName2= ["Rio","Denver","Professor"];
+
+console.log ("Square Bracket Array "+seriesName2[0])
+
+
+let seriesName3:Array<string>               // Generic Type Array 
+seriesName3 = ["Professor","Rio","Denver"];
+
+console.log ("Generic Type Array "+seriesName3[1])
+
+let seriesName:Array<string>
+seriesName= ["Rio","Denver","Professor"];
+
+console.log ("Good Morning "+seriesName[2]);
+
+// Access Array Loop
+
+let friends:string[] = ["Alex","Balaji","Chitra","Durai"];
+
+for (var list in friends){
+
+  console.log ("Hello "+friends[list]+" Access Array Loop")
+}
+
+for (var i=0 ; i<friends.length ; i++){
+
+  console.log ("Hi "+friends[i]);
+}
+
+
+let friends2:string[] = ["Rio","Raqul","Denver","Narobi"]
+let friends3:string[] = ["Rio","Raqul","Denver","Narobi"]
+let friends4:string[] = ["Rio","Raqul","Denver","Narobi"]
+let friends5:string[] = ["Rio","Raqul","Denver","Narobi"]
+
+friends2.push("Last Element Add");  // push() 
+
+console.log(friends2);
+
+friends3.unshift("Frist Element Add"); // unshift ()
+
+console.log (friends3);
+
+friends4.pop();   // pop()
+
+console.log(friends4)
+
+friends5.shift();  // shift()
+
+console.log(friends5);
+
+
+var  text:string  = "Hello Balaji What Are hell Doing ?";
+
+
+console.log(text.indexOf('Balaji'));   // indexOf()
+
+
+
+
+
+
+
+/* ---------------- Array  END  ----------------- */
